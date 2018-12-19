ab2f7d658b554889afcec6933a08b3b69e411c44
Warakia <jcabral@id.uff.br>
2018-12-19 02:24:30 -0200

    Script de apoio para criacao do ambiente

---
+++ partOne.sh
@@ -0,0 +1,29 @@
+#Docker
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" -y
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+#Docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+#Git
+sudo apt-get update
+sudo apt-get install git-core
+#Call next script
+git clone https://github.com/abel-cabral/deploy-rails-with-docker
+cd deploy-rails-with-docker
+chmod 777 partTwo.sh
+/script.sh
